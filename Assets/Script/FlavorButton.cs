1d04f53823ab2e2be53ee228160b77a31073a16b
ZyrearUni <zyreargonzalez@correo.ugr.es>
2026-01-31 15:02:50 +0100

    Flavor text funciona yay

--- Assets/Script/FlavorButton.cs
+++ Assets/Script/FlavorButton.cs
@@ -5,14 +5,18 @@ public class FlavorButton : MonoBehaviour
 {
     public string flavorText;
     public TextMeshProUGUI textContainer;
-    private const int DELAY = 3000;
+    private const float DELAY = 2f;
 
     public void OnClick()
     {
         textContainer.text = flavorText;
         textContainer.enabled = true;
 
-        System.Threading.Thread.Sleep(DELAY);
+        Invoke("DisableText", DELAY);
+    }
+
+    private void DisableText()
+    {
         textContainer.enabled = false;
     }
 }
