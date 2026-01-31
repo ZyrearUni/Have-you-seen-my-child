0149c46dade0f473e4d8c0580348bb346a12095a
ZyrearUni <zyreargonzalez@correo.ugr.es>
2026-01-31 14:43:52 +0100

    Boton funsiona

--- Assets/Script/FlavorButton.cs
+++ Assets/Script/FlavorButton.cs
@@ -4,15 +4,15 @@ using TMPro;
 public class FlavorButton : MonoBehaviour
 {
     public string flavorText;
-    public TextMeshPro textContainer;
+    public TextMeshProUGUI textContainer;
     private const int DELAY = 3000;
 
     public void OnClick()
     {
-        textContainer.text = flavorText; 
-        textContainer.SetEnabled(true);
+        textContainer.text = flavorText;
+        textContainer.enabled = true;
 
         System.Threading.Thread.Sleep(DELAY);
-        //textContainer.SetActive(false);
+        textContainer.enabled = false;
     }
 }
