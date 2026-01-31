aac9f050ba68669dbbdb022bdfe2d7729925b784
ZyrearUni <zyreargonzalez@correo.ugr.es>
2026-01-31 19:51:30 +0100

    Configurados los botones de la cocina

--- Assets/Script/bag.cs
+++ Assets/Script/bag.cs
@@ -3,12 +3,36 @@ using System;
 
 public class Bag : MonoBehaviour
 {
+    private bool hasGocata = false;
+    private bool hasMansana = false;
+    private bool hasMedisina = false;
+
     public event Action MadeLunch; //objective completed
+
+    public Sprite fullBag;
 
     public void OnClick()
     {
-        MadeLunch.Invoke();
-        transform.parent.gameObject.SetActive(false);
+        if (hasGocata && hasMansana && hasMedisina)
+        {
+            MadeLunch.Invoke();
+            transform.parent.GetComponent<SpriteRenderer>().sprite = fullBag;
+        }
+    }
+
+    public void Gocata()
+    {
+        hasGocata = true;
+    }
+
+    public void Mansana()
+    {
+        hasMansana = true;
+    }
+
+    public void Medisina()
+    {
+        hasMedisina = true;
     }
 
 }
