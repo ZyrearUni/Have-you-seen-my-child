f6c7a0d385908fc4355e73b7917013ec9990015f
ZyrearUni <zyreargonzalez@correo.ugr.es>
2026-01-31 16:48:03 +0100

    Cosillas de la máscara

--- Assets/Script/Mask.cs
+++ Assets/Script/Mask.cs
@@ -4,15 +4,31 @@ using System.Collections.Generic;
 public class Mask : MonoBehaviour
 {
     public bool isMaskOn = false;
-    public List<GameObject> buttons;    
+    public List<GameObject> maskedItems;
+    public List<GameObject> unmaskedItems;
+    public List<GameObject> spriteChange;
+
+    public List<Sprite> sprites;
 
     public void OnClick()
     {
         isMaskOn = !isMaskOn;
 
-        for (int i = 0; i < buttons.Count; i++)
+        for (int i = 0; i < maskedItems.Count; i++)
         {
-            buttons[i].SetActive(isMaskOn);
+            maskedItems[i].SetActive(isMaskOn);
         }
+
+        for (int i = 0;i < unmaskedItems.Count; i++) 
+        { 
+            unmaskedItems[i].SetActive(!isMaskOn); 
+        }
+        /*
+         * TODO: Make this shit work well
+         * 
+        for (int i = 0; i < spriteChange.Count; i++)
+        {
+            spriteChange[i].GetComponent<SpriteRenderer>().sprite = sprites[i];
+        }*/
     }
 }
--- Assets/Script/obtainmask.cs
+++ Assets/Script/obtainmask.cs
@@ -2,16 +2,14 @@ using UnityEngine;
 
 public class obtainmask : MonoBehaviour
 {
-    public GameObject esteBoton;
     public GameObject botonMascara;
     public GameObject botonSalirHabitacion;
 
     public void OnClick()
     {
-        esteBoton.SetActive(false);
-
         botonMascara.SetActive(true);
         botonSalirHabitacion.SetActive(true);
-        
+
+        Destroy(transform.parent.gameObject);
     }
 }
