86b1d050c2c41b9df401f58ae02a67fb49984d72
FernandoS <fernandosun62@gmail.com>
2026-01-31 18:56:09 +0100

    hacer la animacion de poner mascara y quitarla

---
+++ Assets/Script/Animacion.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class Animacion : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public void Desactivar()
+    {
+        gameObject.SetActive(false);
+    }
+}
--- Assets/Script/obtainmask.cs
+++ Assets/Script/obtainmask.cs
@@ -1,15 +1,15 @@
 using UnityEngine;
-
+using System.Collections;
+using UnityEngine.UI;
 public class obtainmask : MonoBehaviour
 {
     public GameObject botonMascara;
     public GameObject botonSalirHabitacion;
-
     public void OnClick()
     {
         botonMascara.SetActive(true);
         botonSalirHabitacion.SetActive(true);
-
+        
         Destroy(transform.parent.gameObject);
     }
 }
