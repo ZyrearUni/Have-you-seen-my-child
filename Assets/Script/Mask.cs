530e614192f09ba3bf342b41dacd1417f36aecd6
FernandoS <fernandosun62@gmail.com>
2026-01-31 18:09:37 +0100

    intento de poner la animacion para la mascara

--- Assets/Script/Mask.cs
+++ Assets/Script/Mask.cs
@@ -9,19 +9,28 @@ public class Mask : MonoBehaviour
     public List<GameObject> spriteChange;
 
     public List<Sprite> sprites;
-
+    public Animator anim;
     public void OnClick()
     {
         isMaskOn = !isMaskOn;
 
+        Invoke("ChangeSprites", 2f);
+    }
+
+    private void ChangeSprites()
+    {
         for (int i = 0; i < maskedItems.Count; i++)
         {
             maskedItems[i].SetActive(isMaskOn);
         }
 
-        for (int i = 0;i < unmaskedItems.Count; i++) 
-        { 
-            unmaskedItems[i].SetActive(!isMaskOn); 
+        for (int i = 0; i < unmaskedItems.Count; i++)
+        {
+            unmaskedItems[i].SetActive(!isMaskOn);
+        }
+        if (isMaskOn == true)
+        {
+         anim.Play("PonerMascara");
         }
         /*
          * TODO: Make this shit work well
